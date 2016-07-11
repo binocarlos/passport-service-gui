8878943c96123b3338690c67fd6618677b84f3d9
Kai Davenport <kaiyadavenport@gmail.com>
2016-07-11 20:34:59 +0100

    test for api reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,58 +1,120 @@
 import update from 'react/lib/update'
+import * as actions from '../src/actions'
+
+function defaultFormState(){
+  return {
+    data:{},
+    meta:{}
+  }
+}
+
+function defaultApiState(){
+  return {
+    url:null,
+    loading:false,
+    loaded:false,
+    error:null,
+    data:null
+  } 
+}
 
 export const initialState = {
   forms:{
-    login:{
-      data:{},
-      meta:{}
-    },
-    register:{
-      data:{},
-      meta:{}
-    }
+    login:defaultFormState(),
+    register:defaultFormState()
   },
   api:{
-    status:{
-      url:null,
-      loading:false,
-      loaded:false,
-      error:null,
-      data:null
-    },
-    login:{
-      url:null,
-      loading:false,
-      loaded:false,
-      error:null,
-      data:null
-    },
-    register:{
-      url:null,
-      loading:false,
-      loaded:false,
-      error:null,
-      data:null
-    }
+    status:defaultApiState(),
+    login:defaultApiState(),
+    register:defaultApiState()
   }
+}
+
+function requestHandler(name){
+  return (state, action) => {
+    return update(state, {
+      api: {
+        [name]: {
+          $merge:{
+            url:action.url,
+            loading:true,
+            loaded:false,
+            error:null,
+            data:null
+          }
+        }
+      }
+    })
+  }
+}
+
+function responseHandler(name, state, action){
+  return (state, action) => {
+    return update(state, {
+      api: {
+        [name]: {
+          $merge:{
+            loading:false,
+            loaded:true,
+            error:null,
+            data:action.data
+          }
+        }
+      }
+    })
+  }
+}
+
+function errorHandler(name, state, action){
+  return (state, action) => {
+    return update(state, {
+      api: {
+        [name]: {
+          $merge:{
+            loading:false,
+            loaded:true,
+            error:action.error,
+            data:null
+          }
+        }
+      }
+    })
+  }
+}
+
+function formHandler(state, action){
+  return update(state, {
+    forms: {
+      [action.name]: {
+        $set:{
+          data:action.data,
+          meta:action.meta  
+        }
+      }
+    }
+  })
+}
+
+function getHandlers(name){
+  const uppername = name.toUpperCase()
+  return {
+    ['PASSPORT_' + uppername + '_REQUEST']:requestHandler(name),
+    ['PASSPORT_' + uppername + '_RESPONSE']:responseHandler(name),
+    ['PASSPORT_' + uppername + '_ERROR']:errorHandler(name)
+  }
+}
+
+const handlers = {
+  [actions.PASSPORT_FORM_UPDATE]:formHandler,    
+  ...getHandlers('login'),
+  ...getHandlers('register'),
+  ...getHandlers('status')
 }
 
 export default function reducer(state = initialState, action = {}) {
   
-  switch (action.type) {
-    case 'PASSPORT_FORM_UPDATE':
+  return handlers[action.type] ? 
+    handlers[action.type](state, action) :
+    state
 
-      return update(state, {
-        forms: {
-          [action.name]: {
-            $set:{
-              data:action.data,
-              meta:action.meta  
-            }
-          }
-        }
-      })
-      
-    default:
-      return state
-  }
 }
--- test/reducer.spec.js
+++ test/reducer.spec.js
@@ -2,6 +2,8 @@ import expect from 'expect'
 import reducer, { initialState } from '../src/reducer'
 import * as actions from '../src/actions'
 
+const URL = 'http://127.0.0.1/v1/auth/status'
+
 describe('reducer', () => {
   it('should return the initial state', () => {
 
@@ -35,6 +37,49 @@ describe('reducer', () => {
     expect(state.forms.login.data).toEqual(data)
     expect(state.forms.login.meta).toEqual(meta)
 
+  })
+
+  it('should handle a status api request', () => {
+
+    const state = reducer(undefined, {
+      type:actions.PASSPORT_LOGIN_REQUEST,
+      url:URL
+    })
+
+    expect(state.api.login.loading).toBe(true)
+    expect(state.api.login.loaded).toBe(false)
+    expect(state.api.login.url).toBe(URL)
+  })
+
+  it('should handle a status api response', () => {
+    
+    const DATA = {
+      loggedIn:false
+    }
+    const state = reducer(undefined, {
+      type:actions.PASSPORT_LOGIN_RESPONSE,
+      data:DATA
+    })
+
+    expect(state.api.login.loading).toBe(false)
+    expect(state.api.login.loaded).toBe(true)
+    expect(state.api.login.data).toEqual(DATA)
+    expect(state.api.login.error).toEqual(null)
+  })
+
+  it('should handle a status api error', () => {
+    
+    const ERROR = "connection error"
+      
+    const state = reducer(undefined, {
+      type:actions.PASSPORT_LOGIN_ERROR,
+      error:ERROR
+    })
+
+    expect(state.api.login.loading).toBe(false)
+    expect(state.api.login.loaded).toBe(true)
+    expect(state.api.login.data).toEqual(null)
+    expect(state.api.login.error).toEqual(ERROR)
   })
 
 })
